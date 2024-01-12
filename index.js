61c07d710268e41a4635c8e3c8ccfc610afaa7d2
HuyTrongLe <huytronglee@gmail.com>
2024-01-12 22:04:33 +0700

    init

---
+++ index.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const Book = require('./models/books');
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.set('strictQuery', false);
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI)
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+app.get('/', (req, res) => {
+    res.send({ title: 'Books' });
+});
+
+
+app.get('/add-note', async (req, res) => {
+    try {
+        await Book.insertMany([
+            {
+                title: "Sons of",
+                body: "Body of",
+            },
+            {
+                title: "Game of",
+                body: "Body of",
+            }
+        ]);
+    } catch (error) {
+        console.log("err", + error);
+
+    }
+})
+
+app.get('/book', async (req, res) => {
+    const book = await Book.find();
+    if (book) {
+        res.json(book)
+    } else {
+        res.send("Something went wrong.");
+    }
+});
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}`)
+    })
+});
